27acb99e7531faaf1e5c8ae75b76468bbf214442
Lee Sheppard <lee@leesheppard.com>
2025-07-05 21:25:38 +1000

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,erb,md}",
+    "./frontend/javascript/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
